13f611bbdf5959e49185a72a53f2c60ee720a043
cyren <cyrenarkade@gmail.com>
2026-03-23 13:08:23 -0500

    add cuda cachix

--- nixos/system.nix
+++ nixos/system.nix
@@ -18,10 +18,15 @@
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
 
-    # Hyprland cachix
-    substituters = ["https://hyprland.cachix.org"];
+    substituters = [
+      "https://hyprland.cachix.org"
+      https://cache.nixos-cuda.org
+    ];
     trusted-substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+    ];
   };
 
   # ============= #
